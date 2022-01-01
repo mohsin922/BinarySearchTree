7fae7ea9d0c36b74d5eb241f2b74791243a53f64
peermohsin22 <peermohsin22@gmail.com>
2022-01-01 21:25:01 +0530

    Adding Nodes in BST with Size Of BST

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,7 +6,22 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Binary Search Tree!");
+            Console.WriteLine("BINARY SEARCH TREE");
+            SearchTree<int> binarySearchTree = new SearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Insert(22);
+            binarySearchTree.Insert(40);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(16);
+            binarySearchTree.Insert(60);
+            binarySearchTree.Insert(95);
+            binarySearchTree.Insert(65);
+            binarySearchTree.Insert(63);
+            binarySearchTree.Insert(67);
+            binarySearchTree.Display();
+            binarySearchTree.GetSize();
         }
     }
 }
---
+++ BinarySearchTree/SearchTree.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class SearchTree<T> where T : IComparable<T>
+    {
+        public T nodeData { get; set; }
+        public SearchTree<T> leftTree { get; set; }
+
+        public SearchTree<T> rightTree { get; set; }
+
+        public SearchTree(T data)
+        {
+            this.nodeData = data;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+
+        public void Insert(T item)
+        {
+            T NodeVal = this.nodeData;
+            if ((NodeVal.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new SearchTree<T>(item);
+                }
+                else
+                {
+                    this.leftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new SearchTree<T>(item);
+                }
+                else
+                {
+                    this.rightTree.Insert(item);
+                }
+            }
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size of Tree " + " " + (1 + this.leftCount + this.rightCount));
+        }
+
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.nodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+        }
+
+    }
+}
